7c12b1a893df2c427855ca290d9d0d9fda250992
evelynmk03 <evelynmk03@gmail.com>
2026-04-16 11:04:11 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.store = []
+        self.minstore = []
+        
+
+    def push(self, val: int) -> None:
+        self.store.append(val)
+        if not self.minstore or val <= self.minstore[-1]:
+            self.minstore.append(val)
+
+        
+
+    def pop(self) -> None:
+        if self.minstore[-1] == self.store[-1]:
+            self.minstore.pop()
+        self.store.pop()
+        
+
+    def top(self) -> int:
+        return self.store[-1]
+        
+
+    def getMin(self) -> int:
+            return self.minstore[-1]
+        
