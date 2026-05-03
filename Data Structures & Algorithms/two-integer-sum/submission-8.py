eb5b13a99affe140de8e27d30800c76b130ff161
evelynmk03 <evelynmk03@gmail.com>
2026-05-03 13:44:06 -0700

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        map = {}
+
+        for i, j in enumerate(nums):
+            map[j] = i
+
+        for i in range(len(nums)):
+            j = target - nums[i]
+            if j in map:
+                if j != i and map[j] != i:
+                    return sorted([i,map[j]])
+                continue
+
+
+
+            
+       
